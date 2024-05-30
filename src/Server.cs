fb5006129e4760cc5c1681b28c8dd377d797ab5d
Livia Benedicto <livia.benedicto@hotmail.com>
2024-05-30 09:34:15 -0300

    Extract URL path

--- src/Server.cs
+++ src/Server.cs
@@ -1,12 +1,36 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+using static System.Net.WebRequestMethods;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 
-Socket socket = server.AcceptSocket();
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n\n"));
+try
+{
+    Socket socket = await server.AcceptSocketAsync();
+    
+    // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
+    byte[] responseBuffer = new byte[1024];
+    int _ = await socket.ReceiveAsync(responseBuffer);
+    string[] rows = ASCIIEncoding.UTF8.GetString(responseBuffer).Split("\r\n");
+
+    // GET /index.html HTTP/1.1
+    string[] firstSlipt = rows[0].Split(" ");
+    var (method, path, version) = (firstSlipt[0], firstSlipt[1], firstSlipt[2]);
+    Console.WriteLine($"Method: {method}, Path: {path}, HTTP Version: {version}");
+
+    // HTTP/1.1 404 Not Found\r\n\r\n
+    string okResponse = $"{version} 200 OK\r\n\r\n";
+    string notFoundResponse = $"{version} 400 Not Found\r\n\r\n";
+
+    string response = path == "/" ? okResponse : notFoundResponse;
+    Console.WriteLine($"Response: {response}");
+
+    await socket.SendAsync(ASCIIEncoding.UTF8.GetBytes(response));
+}
+catch (Exception e) { Console.WriteLine(e.Message); }
+
+server.Stop();
+server.Dispose();
